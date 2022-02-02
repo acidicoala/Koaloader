5816807c494399fa73347646fed984bb009d9adf
acidicoala <67734819+acidicoala@users.noreply.github.com>
2022-02-02 12:10:37 +0200

    Changed config & log directory

--- src/koaloader/koaloader.cpp
+++ src/koaloader/koaloader.cpp
@@ -2,6 +2,7 @@
 #include "config/config.hpp"
 #include "logger/logger.hpp"
 #include "win_util/win_util.hpp"
+#include "util/util.hpp"
 
 using namespace koalabox;
 
@@ -9,24 +10,20 @@ HMODULE koaloader::original_module = nullptr;
 
 Vector<HMODULE> injected_modules;
 
-Path get_system_module_path(const String& name) {
-    TCHAR path[MAX_PATH];
-    GetSystemDirectory(path, MAX_PATH);
-    return std::filesystem::absolute(path) / name;
-}
-
 void koaloader::init(HMODULE self_module) {
     DisableThreadLibraryCalls(self_module);
 
-    auto config = config::read("Koaloader.json");
+    auto self_directory = util::get_module_dir(self_module);
+
+    auto config = config::read(self_directory / "Koaloader.json");
 
     if (config.logging) {
-        logger::init("Koaloader.log");
+        logger::init(self_directory / "Koaloader.log");
     }
 
     logger::info("🐨 Koaloader 📥 v{}", PROJECT_VERSION);
 
-    auto original_module_path = get_system_module_path(DLL_NAME".dll");
+    auto original_module_path = win_util::get_system_directory() / DLL_NAME".dll";
     original_module = win_util::load_library(original_module_path);
 
     for (const auto& module: config.modules) {
