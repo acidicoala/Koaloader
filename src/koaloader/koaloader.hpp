25ebb371388fdb22976a31c4eccb6df0bfe46541
acidicoala <67734819+acidicoala@users.noreply.github.com>
2022-02-04 13:04:16 +0200

    Refactored FnCast

--- src/koaloader/koaloader.hpp
+++ src/koaloader/koaloader.hpp
@@ -1,21 +1,16 @@
 #pragma once
 
+#include "koalabox/util/util.hpp"
+
 #include <build_config.h>
 
 #include <boost/preprocessor/tuple/to_seq.hpp>
 #include <boost/preprocessor/seq/for_each_i.hpp>
 #include <boost/preprocessor/comma_if.hpp>
 
-#include <Windows.h>
-
 namespace koaloader {
 
     extern HMODULE original_module;
-
-    template<typename T>
-    T FnCast(FARPROC fnToCast, T) {
-        return (T) fnToCast;
-    }
 
     void init(HMODULE self_module);
 
@@ -37,6 +32,6 @@ namespace koaloader {
 #define FUNC_IMPL(TYPE, NAME, ...) \
 extern "C" BOOL __cdecl __##NAME(BOOST_PP_SEQ_FOR_EACH_I(WRAP_declare_param, ~, BOOST_PP_TUPLE_TO_SEQ((__VA_ARGS__)))) { \
     __pragma(comment(linker, "/EXPORT:"#NAME"=__" PREFIX #NAME)) \
-    static auto func = FnCast(GetProcAddress(original_module, #NAME), __##NAME); \
+    static auto func =  koalabox::util::get_procedure(original_module, #NAME,  __##NAME); \
     return func(BOOST_PP_SEQ_FOR_EACH_I(WRAP_forward_param, ~, BOOST_PP_TUPLE_TO_SEQ((__VA_ARGS__)))); \
 }
