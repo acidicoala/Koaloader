ea8d8037af7461a10a02376063c15984a2bc298b
acidicoala <67734819+acidicoala@users.noreply.github.com>
2022-02-23 22:16:09 +0200

    Refactored CI workflow

--- build.ps1
+++ build.ps1
@@ -1,33 +1,18 @@
 # Usage examples:
-#   build.ps1 32 version Debug
-#   build.ps1 64 xinput9_1_0 Release
-#   build.ps1 64 dinput8 Release
-
-$arch = $args[0]
-if ($arch -notmatch '^(32|64)$') {
-    throw "Invalid architecute. Expected: '32' or '64'. Got: '$arch'"
-}
-$platform = If ($arch -eq '32') { 'Win32' } Else { 'x64' }
-
-$proxy = $args[1]
-if ($proxy -eq '') {
-    throw "Proxy DLL cannot be empty."
-}
-
-$config = $args[2]
-if ($config -notmatch '^(Debug|Release|RelWithDebInfo)$') {
-    throw "Invalid architecute. Expected: 'Debug' or 'Release' or 'RelWithDebInfo'. Got: '$config'"
-}
+#   build.ps1 32 Release dinput8
+#   build.ps1 64 Release version
 
 Set-Location (Get-Item $PSScriptRoot)
 
-$Env:BUILD_DIR = "build\$arch\$proxy"
-if ($config -eq 'Debug') {
-    $Env:VERSION_SUFFIX = "-debug"
+. ./KoalaBox/build.ps1 @args
+
+$proxy = $args[2]
+if ($proxy -eq '') {
+    throw "Proxy DLL cannot be empty."
+} else {
+    $Env:PROXY_DLL = $proxy
 }
 
-$Env:PROXY_DLL = $proxy
+$Env:BUILD_DIR = "build\$arch\$proxy"
 
-cmake -G "Visual Studio 17 2022" -A $platform -B "$Env:BUILD_DIR"
-
-cmake --build "$Env:BUILD_DIR" --config $config
+Build-Project
